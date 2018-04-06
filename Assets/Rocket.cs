a4a052b70203b3b352485936fc79a2c62ae65e43
LAPTOP-KP6TNU2F\arran games <arranbb2004@gmail.com>
2018-04-06 08:49:32 +0100

    can fly now

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    
+    // Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Processinput();
+	}
+
+    private void Processinput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("turning left");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("turning right");
+        }
+    }
+}
