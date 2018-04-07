0c7443f46bc5fa677d7fc4f28cdb10f878d0c62c
LAPTOP-KP6TNU2F\arran games <arranbb2004@gmail.com>
2018-04-07 10:16:21 +0100

    added audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
     
     // Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
-	}
+        audioSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,14 +24,22 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up*Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward*Time.deltaTime);
+            transform.Rotate(Vector3.forward*Time.deltaTime*50);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward*Time.deltaTime);
+            transform.Rotate(-Vector3.forward*Time.deltaTime*50);
         }
     }
 }
